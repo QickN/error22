56e3d08528b71c70ad43e140fb092f1cbec44862
QickN <100318304+QickN@users.noreply.github.com>
2022-12-05 23:11:17 -0700

    added comments

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-
 //Imports 
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
 import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js'
 import { FontLoader } from 'https://unpkg.com/three@0.141.0/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'https://unpkg.com/three@0.141.0/examples/jsm/geometries/TextGeometry.js'
 
-
 //Setup camera and scene with the size of the window
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000);
 
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg')
 })
 
-const white = new THREE.Color("rgb(255,255,255)")
+const white = new THREE.Color( "rgb(255,255,255)" )
 
 renderer.setPixelRatio( window.devicePixelRatio )
 renderer.setSize( window.innerWidth, window.innerHeight)
@@ -27,7 +24,7 @@ renderer.render(scene, camera);
 
 const donut = new THREE.TorusGeometry(10,3,10,90)
 
-const donutMat = new THREE.MeshBasicMaterial( {color: 0xFFFFF, wireframe: true})
+const donutMat = new THREE.MeshBasicMaterial( {color: 0xFFFFF, wireframe: true} )
 
 const donutMesh = new THREE.Mesh(donut, donutMat)
 
